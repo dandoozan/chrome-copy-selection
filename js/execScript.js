b889afd0f495024b050dfb42c4eb3fc19a218efb
Dan Doozan <dandoozan@gmail.com>
2018-07-28 15:16:07 -0700

    Initial commit

---
+++ js/execScript.js
@@ -0,0 +1,26 @@
+// console.log('dpd here');
+
+function getSelectedText() {
+    return window.getSelection().toString();
+}
+
+function copy() {
+    document.execCommand('copy');
+}
+
+function copyTextIfAnyIsSelectedAndDisplayMessage() {
+    let selectedText = getSelectedText();
+    // console.log('selectedText=',selectedText);
+    if (selectedText) {
+        //copy the selected text
+        copy();
+
+        //display message
+    }
+}
+
+//listen for 'mouseup' and 'keyup' events
+document.addEventListener('mouseup', copyTextIfAnyIsSelectedAndDisplayMessage);
+document.addEventListener('keyup', copyTextIfAnyIsSelectedAndDisplayMessage);
+
+
