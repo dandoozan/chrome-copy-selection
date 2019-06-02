18cba766ab56b3b563e625e2fe92853b24284f99
Dan Doozan <dandoozan@gmail.com>
2019-06-02 16:13:09 -0700

    Copy the url of the page if a user presses copy and no text is selected on the page

--- js/execScript.js
+++ js/execScript.js
@@ -1,7 +1,9 @@
 (() => {
-    const URL = window.location.href;
-
     let currentSelectedText = '';
+
+    function getPageUrl() {
+        return window.location.href;
+    }
 
     function getSelectedText() {
         return window.getSelection().toString();
@@ -28,32 +30,61 @@
         return isInputElement(getFocusedElement());
     }
 
-    function copyTextIfApplicableAndDisplayMessage(ev) {
+    function copyTextIfApplicableAndDisplayMessage() {
         // if (!inputFieldHasFocus()) {
-            let selectedText = getSelectedText();
-            console.log(`selectedText="${selectedText}"`);
+        let selectedText = getSelectedText();
+        console.log(`selectedText="${selectedText}"`);
 
-            currentSelectedText = selectedText;
-            if (selectedText) {
-                //copy the selected text
-                copy();
+        currentSelectedText = selectedText;
+        if (selectedText) {
+            //copy the selected text
+            copy();
 
-                //display toast
-                notifier.notify(`Copied: "${selectedText}"`);
-            }
+            //display toast
+            notifier.notify(`Copied: "${selectedText}"`);
+        }
         // }
     }
 
-    function handleKeyUp(ev) {
+    function handleMouseUp(event) {
+        copyTextIfApplicableAndDisplayMessage();
+    }
+
+    function handleKeyUp(event) {
         let selectedText = getSelectedText();
+
+        //add this check to prevent the toast from showing when the
+        //user makes unrelated keyboard presses (eg. pressing "down" to
+        //scroll down the page) (without this check, the toast will show
+        //up everytime the user presses "down")
         if (selectedText !== currentSelectedText) {
-            copyTextIfApplicableAndDisplayMessage(ev);
+            copyTextIfApplicableAndDisplayMessage();
+        }
+    }
+
+    function handleCopyEvent(event) {
+        const selectedText = getSelectedText();
+
+        //if there is no selected text on the page, then copy the url
+        if (!selectedText) {
+            let textToCopy = getPageUrl();
+
+            //write the url to the clipboard
+            event.clipboardData.setData('text/plain', textToCopy);
+
+            //display toast
+            notifier.notify(`Copied: "${textToCopy}"`);
+
+            //prevent default to prevent the browser from executing a "copy" command
+            event.preventDefault();
         }
     }
 
     //----------main----------
     const notifier = new Notifier();
 
-    document.addEventListener('mouseup', copyTextIfApplicableAndDisplayMessage);
+    //add event listeners
+    document.addEventListener('mouseup', handleMouseUp);
     document.addEventListener('keyup', handleKeyUp);
+    document.addEventListener('copy', handleCopyEvent);
 })();
