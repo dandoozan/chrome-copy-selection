508223258ba05496856f84cfb55b59c74e710ee4
Dan Doozan <dandoozan@gmail.com>
2019-06-02 15:51:34 -0700

    Remove code dealing with google docs since it's unnecessary

--- js/execScript.js
+++ js/execScript.js
@@ -41,16 +41,6 @@
     //     document.body.appendChild(div);
     // }
 
-    function isGoogleSheet() {
-        return URL.startsWith('https://docs.google.com/spreadsheets/d/');
-    }
-
-    function shouldIgnoreDueToSpecialCase(selectedText) {
-        //ignore when an empty cell is clicked on in a google sheet (in this case, the
-        //the page thinks that a newline has been selected, but really nothing has been selected)
-        return isGoogleSheet(URL) && selectedText === '\n';
-    }
-
     function copyTextIfApplicableAndDisplayMessage(ev) {
         // if (!inputFieldHasFocus()) {
             let selectedText = getSelectedText();
@@ -58,12 +48,6 @@
 
             currentSelectedText = selectedText;
             if (selectedText) {
-
-                //handle special cases
-                if (shouldIgnoreDueToSpecialCase(selectedText)) {
-                    return;
-                }
-
                 //copy the selected text
                 copy();
 
