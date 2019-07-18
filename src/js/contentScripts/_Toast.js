8b85de9f7bda26b9c489f8cd23ee935718a1e137
Dan Doozan <dandoozan@gmail.com>
2019-07-17 17:01:49 -0700

    Convert toast class to a module

--- src/js/contentScripts/_Toast.js
+++ src/js/contentScripts/_Toast.js
@@ -1,34 +1,30 @@
-export class Toast {
-    constructor() {
-        this.ELEMENT_ID = 'crx_toast';
+const ELEMENT_ID = 'crx_toast';
+
+function createToastElement(body) {
+    //create the element
+    let div = document.createElement('div');
+    div.id = ELEMENT_ID;
+    div.innerHTML = body;
+
+    //add the click listener to remove it when clicked
+    div.addEventListener('click', removeToastElement);
+
+    //add it to the page
+    document.body.appendChild(div);
+}
+
+function removeToastElement() {
+    let element = document.getElementById(ELEMENT_ID);
+    if (element) {
+        element.remove();
     }
+}
 
-    createToastElement(body) {
-        //create the element
-        let div = document.createElement('div');
-        div.id = this.ELEMENT_ID;
-        div.innerHTML = body;
+export function showToast(msg) {
+    //remove previous toast element if it exists (which will happen if text has
+    //previously been selected on the page before)
+    removeToastElement();
 
-        //add the click listener to remove it when clicked
-        div.addEventListener('click', this.removeToastElement.bind(this));
-
-        //add it to the page
-        document.body.appendChild(div);
-    }
-
-    removeToastElement() {
-        let element = document.getElementById(this.ELEMENT_ID);
-        if (element) {
-            element.remove();
-        }
-    }
-
-    show(msg) {
-        //remove previous toast element if it exists (which will happen if text has
-        //previously been selected on the page before)
-        this.removeToastElement();
-
-        //create new toast element
-        this.createToastElement(msg);
-    }
+    //create new toast element
+    createToastElement(msg);
 }
--- src/js/contentScripts/_main.js
+++ src/js/contentScripts/_main.js
@@ -1,92 +1,88 @@
-import { Toast } from './_Toast';
+import { showToast } from './_toast';
 
-(function main() {
-    function getPageUrl() {
-        return window.location.href;
-    }
+let currentSelectedText = '';
 
-    function getSelectedText() {
-        return window.getSelection().toString();
-    }
+function getPageUrl() {
+    return window.location.href;
+}
 
-    function copy() {
-        document.execCommand('copy');
-    }
+function getSelectedText() {
+    return window.getSelection().toString();
+}
 
-    function isInputElement(el) {
-        return (
-            el.tagName === 'INPUT' ||
-            el.tagName === 'SELECT' ||
-            el.tagName === 'TEXTAREA' ||
-            el.isContentEditable
-        );
-    }
+function copy() {
+    document.execCommand('copy');
+}
 
-    function getFocusedElement() {
-        return document.activeElement;
-    }
+function isInputElement(el) {
+    return (
+        el.tagName === 'INPUT' ||
+        el.tagName === 'SELECT' ||
+        el.tagName === 'TEXTAREA' ||
+        el.isContentEditable
+    );
+}
 
-    function inputFieldHasFocus() {
-        return isInputElement(getFocusedElement());
-    }
+function getFocusedElement() {
+    return document.activeElement;
+}
 
-    function displayToast(msg) {
-        toast.show(`Copied: "${msg}"`);
-    }
+function inputFieldHasFocus() {
+    return isInputElement(getFocusedElement());
+}
 
-    function copySelectedTextIfApplicable() {
-        if (!inputFieldHasFocus()) {
-            let selectedText = getSelectedText();
+function displayToast(msg) {
+    showToast(`Copied: "${msg}"`);
+}
 
-            //check that selectedText is truthy to ensure we have something to copy AND check
-            //that selectedText != currentSelectedText so that I don't do unnecessary work (of
-            //double copying and also showing the toast twice for the same text); in other
-            //words, if there is text already selected on the page, then it has already been
-            //copied and the user has already been shown a toast about it, so don't show it again
-            console.log(
-                `selectedText="${selectedText}; currentSelectedText="${currentSelectedText}"`
-            );
-            if (selectedText && selectedText !== currentSelectedText) {
-                //copy the selected text
-                copy();
+function copySelectedTextIfApplicable() {
+    if (!inputFieldHasFocus()) {
+        let selectedText = getSelectedText();
 
-                //display toast
-                displayToast(selectedText);
-            }
-
-            currentSelectedText = selectedText;
-        }
-    }
-
-    function handleMouseUp(event) {
-        copySelectedTextIfApplicable();
-    }
-
-    function handleKeyUp(event) {
-        copySelectedTextIfApplicable();
-    }
-
-    function handleCopyEvent(event) {
-        const selectedText = getSelectedText();
-
-        //if there is no selected text on the page, then copy the url
-        if (!selectedText) {
-            //copy the url
-            let textToCopy = getPageUrl();
-            event.clipboardData.setData('text/plain', textToCopy);
+        //check that selectedText is truthy to ensure we have something to copy AND check
+        //that selectedText != currentSelectedText so that I don't do unnecessary work (of
+        //double copying and also showing the toast twice for the same text); in other
+        //words, if there is text already selected on the page, then it has already been
+        //copied and the user has already been shown a toast about it, so don't show it again
+        // console.log(`selectedText="${selectedText}; currentSelectedText="${currentSelectedText}"`);
+        if (selectedText && selectedText !== currentSelectedText) {
+            //copy the selected text
+            copy();
 
             //display toast
-            displayToast(textToCopy);
-
-            //prevent default to prevent the browser from executing a "copy" command
-            event.preventDefault();
+            displayToast(selectedText);
         }
+
+        currentSelectedText = selectedText;
     }
+}
 
-    //----------main----------
-    const toast = new Toast();
-    let currentSelectedText = '';
+function handleMouseUp(event) {
+    copySelectedTextIfApplicable();
+}
 
+function handleKeyUp(event) {
+    copySelectedTextIfApplicable();
+}
+
+function handleCopyEvent(event) {
+    const selectedText = getSelectedText();
+
+    //if there is no selected text on the page, then copy the url
+    if (!selectedText) {
+        //copy the url
+        let textToCopy = getPageUrl();
+        event.clipboardData.setData('text/plain', textToCopy);
+
+        //display toast
+        displayToast(textToCopy);
+
+        //prevent default to prevent the browser from executing a "copy" command
+        event.preventDefault();
+    }
+}
+
+(function main() {
     //add event listeners
     document.addEventListener('mouseup', handleMouseUp);
     document.addEventListener('keyup', handleKeyUp);
