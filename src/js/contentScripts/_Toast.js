1cda561d2e4f79f218850738122b092878b3ffa8
Dan Doozan <dandoozan@gmail.com>
2019-10-05 08:32:17 -0700

    Run prettier across all files

--- src/js/contentScripts/_Toast.js
+++ src/js/contentScripts/_Toast.js
@@ -1,30 +1,30 @@
 const ELEMENT_ID = 'crx_toast';
 
 function createToastElement(body) {
-    //create the element
-    let div = document.createElement('div');
-    div.id = ELEMENT_ID;
-    div.innerHTML = body;
+  //create the element
+  let div = document.createElement('div');
+  div.id = ELEMENT_ID;
+  div.innerHTML = body;
 
-    //add the click listener to remove it when clicked
-    div.addEventListener('click', removeToastElement);
+  //add the click listener to remove it when clicked
+  div.addEventListener('click', removeToastElement);
 
-    //add it to the page
-    document.body.appendChild(div);
+  //add it to the page
+  document.body.appendChild(div);
 }
 
 function removeToastElement() {
-    let element = document.getElementById(ELEMENT_ID);
-    if (element) {
-        element.remove();
-    }
+  let element = document.getElementById(ELEMENT_ID);
+  if (element) {
+    element.remove();
+  }
 }
 
 export function showToast(msg) {
-    //remove previous toast element if it exists (which will happen if text has
-    //previously been selected on the page before)
-    removeToastElement();
+  //remove previous toast element if it exists (which will happen if text has
+  //previously been selected on the page before)
+  removeToastElement();
 
-    //create new toast element
-    createToastElement(msg);
+  //create new toast element
+  createToastElement(msg);
 }
--- src/js/contentScripts/_main.js
+++ src/js/contentScripts/_main.js
@@ -3,81 +3,81 @@ import { showToast } from './_toast';
 let currentSelectedText = '';
 
 function isInputElement(el) {
-    return (
-        el.tagName === 'INPUT' ||
-        el.tagName === 'SELECT' ||
-        el.tagName === 'TEXTAREA' ||
-        el.isContentEditable
-    );
+  return (
+    el.tagName === 'INPUT' ||
+    el.tagName === 'SELECT' ||
+    el.tagName === 'TEXTAREA' ||
+    el.isContentEditable
+  );
 }
 
 function inputFieldHasFocus() {
-    return isInputElement(document.activeElement);
+  return isInputElement(document.activeElement);
 }
 
 function getSelectedText() {
-    return window.getSelection().toString();
+  return window.getSelection().toString();
 }
 
 function displayToast(msg) {
-    showToast(`Copied: "${msg}"`);
+  showToast(`Copied: "${msg}"`);
 }
 
 function copySelectedTextIfApplicable() {
-    if (!inputFieldHasFocus()) {
-        let selectedText = getSelectedText();
+  if (!inputFieldHasFocus()) {
+    let selectedText = getSelectedText();
 
-        //check that selectedText is truthy to ensure we have something to copy AND check
-        //that selectedText != currentSelectedText so that I don't do unnecessary work (of
-        //double copying and also showing the toast twice for the same text); in other
-        //words, if there is text already selected on the page, then it has already been
-        //copied and the user has already been shown a toast about it, so don't show it again
-        // console.log(`selectedText="${selectedText}; currentSelectedText="${currentSelectedText}"`);
-        if (selectedText && selectedText !== currentSelectedText) {
-            //copy the selected text
-            document.execCommand('copy');
+    //check that selectedText is truthy to ensure we have something to copy AND check
+    //that selectedText != currentSelectedText so that I don't do unnecessary work (of
+    //double copying and also showing the toast twice for the same text); in other
+    //words, if there is text already selected on the page, then it has already been
+    //copied and the user has already been shown a toast about it, so don't show it again
+    // console.log(`selectedText="${selectedText}; currentSelectedText="${currentSelectedText}"`);
+    if (selectedText && selectedText !== currentSelectedText) {
+      //copy the selected text
+      document.execCommand('copy');
 
-            //display toast
-            displayToast(selectedText);
-        }
-
-        currentSelectedText = selectedText;
+      //display toast
+      displayToast(selectedText);
     }
+
+    currentSelectedText = selectedText;
+  }
 }
 
 function handleMouseUp(event) {
-    copySelectedTextIfApplicable();
+  copySelectedTextIfApplicable();
 }
 
 function handleKeyUp(event) {
-    copySelectedTextIfApplicable();
+  copySelectedTextIfApplicable();
 }
 
 function handleCopyEvent(event) {
-    const selectedText = getSelectedText();
+  const selectedText = getSelectedText();
 
-    //if there is no selected text on the page, then copy the url
-    if (!selectedText) {
-        let url = window.location.href;
+  //if there is no selected text on the page, then copy the url
+  if (!selectedText) {
+    let url = window.location.href;
 
-        //put the url in an anchor tag so that it's copied as a link to the clipboard (so that
-        //when it's pasted, it gets pasted as a link (instead of just plaintext))
-        event.clipboardData.setData('text/html', `<a href="${url}">${url}</a>`);
+    //put the url in an anchor tag so that it's copied as a link to the clipboard (so that
+    //when it's pasted, it gets pasted as a link (instead of just plaintext))
+    event.clipboardData.setData('text/html', `<a href="${url}">${url}</a>`);
 
-        //fallback to plaintext if the above doesn't work (per MDN recommendation)
-        event.clipboardData.setData('text/plain', url);
+    //fallback to plaintext if the above doesn't work (per MDN recommendation)
+    event.clipboardData.setData('text/plain', url);
 
-        //display toast
-        displayToast(url);
+    //display toast
+    displayToast(url);
 
-        //prevent default to prevent the browser from executing a "copy" command
-        event.preventDefault();
-    }
+    //prevent default to prevent the browser from executing a "copy" command
+    event.preventDefault();
+  }
 }
 
 (function main() {
-    //add event listeners
-    document.addEventListener('mouseup', handleMouseUp);
-    document.addEventListener('keyup', handleKeyUp);
-    document.addEventListener('copy', handleCopyEvent);
+  //add event listeners
+  document.addEventListener('mouseup', handleMouseUp);
+  document.addEventListener('keyup', handleKeyUp);
+  document.addEventListener('copy', handleCopyEvent);
 })();
